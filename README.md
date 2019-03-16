d9d9d89ae20618b56e638c90d8eb0112a5846f1a
Ilya Zvoznikov <ilya.zvoznikov@gmail.com>
2019-03-16 09:02:54 +0700

    create main app logic

--- README.md
+++ README.md
@@ -1,16 +1,33 @@
 # Watcher of Friends Online
 
-[TODO. There will be project description]
+The app "Watcher of Friends Online" allows to detect your friends from [vk.com](https://vk.com) who is online.
 
 # How to Install
 
 Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
 
 ```bash
-pip install -r requirements.txt # alternatively try pip3
+$ pip install -r requirements.txt # alternatively try pip3
 ```
 
 Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
+
+# Quickstart
+
+To use run the app and enter login and password from user account on [vk.com](https://vk.com)
+
+```bash
+$ python vk_friends_online.py
+Input User login:user@gmail.com
+Input User password:
+
+Friends online:
+Георгий Андреевич
+Галина Храмова
+Станислав Брант
+Анастасия Белоусова
+German Iordanskii
+```
 
 # Project Goals
 
--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,29 +1,67 @@
+import getpass
+import requests
 import vk
 
+# чтобы получить app_id,
+# нужно зарегистрировать своё приложение на https://vk.com/dev
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 6900175
 
 
 def get_user_login():
-    pass
+    user_login = input('Input User login:')
+    return user_login
 
 
 def get_user_password():
-    pass
+    password = getpass.getpass('Input User password:')
+    return password
 
 
 def get_online_friends(login, password):
-    session = vk.AuthSession(
-        app_id=APP_ID,
-        user_login=login,
-        user_password=password,
-    )
-    api = vk.API(session)
-    # например, api.friends.get()
+    if not all([login, password]):
+        return None
+
+    try:
+        session = vk.AuthSession(
+            app_id=APP_ID,
+            user_login=login,
+            user_password=password,
+            scope='friends, users'
+        )
+        api = vk.API(session)
+    except (
+            requests.exceptions.ConnectionError,
+            requests.exceptions.ConnectTimeout,
+            vk.exceptions.VkAuthError
+    ):
+        return None
+
+    friends_online_ids = api.friends.getOnline(v=5.92)
+    friends_online_list = [
+        api.users.get(user_ids=user_id, v=5.89)
+        for user_id in friends_online_ids
+    ]
+
+    return friends_online_list
 
 
 def output_friends_to_console(friends_online):
-    pass
+    print('\nFriends online:')
+
+    if not friends_online:
+        exit('No friends online'
+             '\n\nPossible reasons:'
+             '\n\t- all friends offline'
+             '\n\t- login and password are incorrect or empty'
+             '\n\t- Internet connection error'
+             )
+
+    fields = ['first_name', 'last_name', ]
+
+    for friend in friends_online:
+        print(*[friend[0].get(field) for field in fields])
+
 
 if __name__ == '__main__':
     login = get_user_login()
